fcddff183ae70a5dc2b885a2b23d6342d558f990
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-03-09 11:41:42 +1300

    Use a map to return a greeting for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -8,15 +8,21 @@ import (
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Get a greeting message and print it.
-	message, err := greetings.Hello("Bob")
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
 
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println(message)
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
